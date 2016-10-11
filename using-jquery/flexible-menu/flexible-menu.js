fe719a8c74564940bc88a7c87d5f548d9d5be9c6
Rodolfo Costa <rodolfombc@gmail.com>
2016-10-11 01:50:50 -0300

    Adding radio button options to change between number of rows and columns

--- using-jquery/flexible-menu/flexible-menu.js
+++ using-jquery/flexible-menu/flexible-menu.js
@@ -1,3 +1,6 @@
+
+var transitionEvent;
+
 //
 //Method to detect the cross-browser event name of the "transition animation" property 
 //
@@ -20,15 +23,79 @@ function whichTransitionEvent(){
 }
 
 //Getting the current transition animation event name
-var transitionEvent = whichTransitionEvent();
+transitionEvent = whichTransitionEvent();
 
 //
 //Method to detect when the Document Object Model (DOM) is ready for JavaScript code to execute
 //
 $(document).ready(function() 
 {
+	//console.log(transitionEvent);
 	
+	$(".row-option:first").click();
+	$(".column-option:first").click();
+
 });
+
+$(".column-option").on("click", function()
+{
+	//unchecking all radio buttons
+	$(".column-option").each(function( index ) 
+	{
+		//console.log( index + ": " + $( this ).val() );
+		$(this).prop('checked', false); 
+	});
+	$(this).prop('checked', true); //checking the radio button the user chose
+	
+	//Cheking which case was chosen
+	verifyOption($(this).val());	
+	
+	//console.log($(this).val());
+});
+
+
+$(".row-option").on("click", function()
+{
+	//unchecking all radio buttons
+	$(".row-option").each(function( index ) 
+	{
+		//console.log( index + ": " + $( this ).val() );
+		$(this).prop('checked', false); 
+	});
+	$(this).prop('checked', true); //checking the radio button the user chose
+	
+	//Cheking which case was chosen
+	verifyOption($(this).val());	
+	
+	//console.log($(this).val());
+});
+
+
+function verifyOption(radioOption) 
+{
+	switch(radioOption)
+	{
+		case "1row":
+			console.log("set 1 row");
+			break;
+		case "2rows":
+			console.log("set 2 rows");
+			break;
+		case "3rows":
+			console.log("set 3 rows");
+			break;
+		case "1column":
+			console.log("set 1 column");
+			break;
+		case "2columns":
+			console.log("set 2 columns");
+			break;
+		case "3columns":
+			console.log("set 3 columns");
+			break;
+	}
+}
+
 
 function getWindowWidth()
 {
